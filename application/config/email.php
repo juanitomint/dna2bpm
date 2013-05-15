393e9c102a340e88a7567448b396f68dd6d9179d
Diego Otero <daotero@industria.gob.ar>
2013-05-15 11:30:49 -0300

    added missing config folder

---
+++ application/config/email.php
@@ -0,0 +1,15 @@
+<?php
+$config['protocol'] = 'smtp';
+$config['smtp_host'] = 'www.dna2.org';
+$config['smtp_user'] = 'dna2';
+$config['smtp_pass'] = 'novanet';
+$config['smtp_timeout'] = '30';
+$config['protocol'] = 'smtp';
+$config['charset'] = 'UTF-8';
+$config['wordwrap'] = TRUE;
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
