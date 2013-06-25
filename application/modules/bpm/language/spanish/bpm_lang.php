d930820f7906befcb2bdb36f30c2609f05097014
Juan Borda <juanb@mp.gba.gov.ar>
2013-06-25 13:37:47 -0300

    added in module support

---
+++ application/modules/bpm/language/spanish/bpm_lang.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+/* ---WF-Tasks------ */
+$lang["MyTasks"] = 'Mis Tareas';
+$lang["byGroup"] = 'Por Grupo';
+$lang["byStatus"] = 'Por Estado';
+$lang["Pending"] = 'Pendientes';
+$lang["Finished"] = 'Finalizadas';
+$lang["Canceled"] = 'Canceladas';
+$lang["Waiting"] = 'Esperando';
+$lang["Stoped"] = 'Detenida';
+$lang["finishTask"] = 'dar por Finalizada';
+$lang["closeTask"] = 'volver';
+$lang["claim"] = 'reclamar';
+$lang["refuse"] = 'rechazar';
+
+/* --WF-Messages----- */
+$lang['newTask'] = 'Nueva Tarea';
+$lang['newTaskBody'] = "{from_user name} {from_user lastname} te ha asignado una nueva tarea: {shape name}
+    <br/>
+    Haz click <a href='{basedir}/bpm/engine/run/model/{idwf}/{idcase}'>>>>aquí<<<</a> para completarla";
+$lang['lock'] = 'Bloqueo';
+$lang['message'] = 'Mensaje';
+$lang["caseLocked"] = "<i class='icon-lock'></i> Este caso hs sido bloqueado por:<br/> {user_lock}<br/> desde el: {time}";
+$lang["taskLocked"] = "<i class='icon-lock'></i> Esta tarea ha sido bloqueada por:<br/> {user_lock}<br/> desde el: {time}";
+$lang["noMoreTasks"] = "Ud no tiene más tareas pendientes por ahora";
+$lang["caseClosed"] = "El proceso ha finalizado.";
+$lang["closedCases"] = "Casos Cerrados";
+/* --WF-conditions----- */
+$lang['true'] = 'Yes';
+$lang['false'] = 'No';
