a8766efd7873e08a4fc7a8ccc62662499eba5d0f
Juan Borda <juanb@mp.gba.gov.ar>
2013-06-25 13:54:56 -0300

    updated spanish library lang updated login localization

--- application/language/english/login_lang.php
+++ application/language/english/login_lang.php
@@ -5,8 +5,10 @@ if (!defined("BASEPATH"))
 $lang["username"] = "user name";
 $lang["password"] = "password";
 $lang["PageTitle"] = "LogIn";
+$lang['loginMsg'] = 'Please Sign In';
 $lang["PageDescription"] = "Access for registered users.";
 $lang["loginButton"] = "go!";
+$lang["rememberButton"] = "Remember Me";
 $lang["forgotPassword"] = "Forgot your username or password!";
 $lang["nolevel"] = "You don't have permission to access:";
 $lang["nousr"] = "You are not a registered user or you've misstyped either the username or password";
