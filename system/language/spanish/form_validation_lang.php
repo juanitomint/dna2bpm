f157d631b5a2d30e8820bc16715d2dcdf6a36456
Juan Borda <juanb@mp.gba.gov.ar>
2013-07-04 11:24:25 -0300

    added Spanish language support from: http://www.cesardelacruz.es/codeigniter-2-0-2-spanish-pack/

--- system/language/spanish/form_validation_lang.php
+++ system/language/spanish/form_validation_lang.php
@@ -1,28 +1,27 @@
 <?php
 
-$lang['required']			= "The %s field is required.";
-$lang['isset']				= "The %s field must have a value.";
-$lang['valid_email']		= "The %s field must contain a valid email address.";
-$lang['valid_emails']		= "The %s field must contain all valid email addresses.";
-$lang['valid_url']			= "The %s field must contain a valid URL.";
-$lang['valid_ip']			= "The %s field must contain a valid IP.";
-$lang['min_length']			= "The %s field must be at least %s characters in length.";
-$lang['max_length']			= "The %s field can not exceed %s characters in length.";
-$lang['exact_length']		= "The %s field must be exactly %s characters in length.";
-$lang['alpha']				= "The %s field may only contain alphabetical characters.";
-$lang['alpha_numeric']		= "The %s field may only contain alpha-numeric characters.";
-$lang['alpha_dash']			= "The %s field may only contain alpha-numeric characters, underscores, and dashes.";
-$lang['numeric']			= "The %s field must contain only numbers.";
-$lang['is_numeric']			= "The %s field must contain only numeric characters.";
-$lang['integer']			= "The %s field must contain an integer.";
-$lang['regex_match']		= "The %s field is not in the correct format.";
-$lang['matches']			= "The %s field does not match the %s field.";
-$lang['is_unique'] 			= "The %s field must contain a unique value.";
-$lang['is_natural']			= "The %s field must contain only positive numbers.";
-$lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
-$lang['decimal']			= "The %s field must contain a decimal number.";
-$lang['less_than']			= "The %s field must contain a number less than %s.";
-$lang['greater_than']		= "The %s field must contain a number greater than %s.";
+$lang['required']			= "El campo %s es obligatorio.";
+$lang['isset']				= "El campo %s debe contener un valor.";
+$lang['valid_email']		= "El campo %s debe contener una dirección de correo válida.";
+$lang['valid_emails']		= "El campo %s debe contener todas las direcciones de correo válidas.";
+$lang['valid_url']			= "El campo %s debe contener una URL válida.";
+$lang['valid_ip']			= "El campo %s debe contener una dirección IP válida.";
+$lang['min_length']			= "El campo %s debe contener al menos %s caracteres de longitud.";
+$lang['max_length']			= "El campo %s no debe exceder los %s caracteres de longitud.";
+$lang['exact_length']		= "El campo %s debe tener exactamente %s carácteres.";
+$lang['alpha']				= "El campo %s sólo puede contener carácteres alfabéticos.";
+$lang['alpha_numeric']		= "El campo %s sólo puede contener carácteres alfanuméricos.";
+$lang['alpha_dash']			= "El campo %s sólo puede contener carácteres alfanuméricos, guiones bajos '_' o guiones '-'.";
+$lang['numeric']			= "El campo %s sólo puede contener números.";
+$lang['is_numeric']			= "El campo %s sólo puede contener carácteres números.";
+$lang['integer']			= "El campo %s debe contener un número entero.";
+$lang['regex_match']		= "El campo %s no tiene el formato correcto.";
+$lang['matches']			= "El campo %s no concuerda con el campo %s .";
+$lang['is_natural']			= "El campo %s debe contener sólo números positivos.";
+$lang['is_natural_no_zero']	= "El campo %s debe contener un número mayor que 0.";
+$lang['decimal']			= "El campo %s debe contener un número decimal.";
+$lang['less_than']			= "El campo %s debe contener un número menor que %s.";
+$lang['greater_than']		= "El campo %s debe contener un número mayor que %s.";
 
 
 /* End of file form_validation_lang.php */
--- system/language/spanish/profiler_lang.php
+++ system/language/spanish/profiler_lang.php
@@ -9,17 +9,14 @@ $lang['profiler_post_data']		= 'POST DATA';
 $lang['profiler_uri_string']	= 'URI STRING';
 $lang['profiler_memory_usage']	= 'MEMORY USAGE';
 $lang['profiler_config']		= 'CONFIG VARIABLES';
-$lang['profiler_session_data']	= 'SESSION DATA';
 $lang['profiler_headers']		= 'HTTP HEADERS';
-$lang['profiler_no_db']			= 'Database driver is not currently loaded';
-$lang['profiler_no_queries']	= 'No queries were run';
-$lang['profiler_no_post']		= 'No POST data exists';
-$lang['profiler_no_get']		= 'No GET data exists';
-$lang['profiler_no_uri']		= 'No URI data exists';
-$lang['profiler_no_memory']		= 'Memory Usage Unavailable';
-$lang['profiler_no_profiles']	= 'No Profile data - all Profiler sections have been disabled.';
-$lang['profiler_section_hide']	= 'Hide';
-$lang['profiler_section_show']	= 'Show';
+$lang['profiler_no_db']			= 'El driver para Base de Datos no ha sido cargado';
+$lang['profiler_no_queries']	= 'No se han ejecutado consultas';
+$lang['profiler_no_post']		= 'No existen datos de tipo POST';
+$lang['profiler_no_get']		= 'No existen datos de tipo GET';
+$lang['profiler_no_uri']		= 'No existen datos de tipo URI';
+$lang['profiler_no_memory']		= 'Uso de memoria no disponible';
+$lang['profiler_no_profiles']	= 'No hay datos de perfil - se desabilitan todas las secciones del perfil.';
 
 /* End of file profiler_lang.php */
 /* Location: ./system/language/english/profiler_lang.php */
