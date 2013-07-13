551ef6c53eec5d006f675099e38a3c0fd11216fb
juanb <juanb@lmde>
2013-07-13 11:10:30 -0300

    commented ldap plugin Signed-off-by:juanb <juanb@lmde>

--- application/modules/user/config/autoload.php
+++ application/modules/user/config/autoload.php
@@ -4,7 +4,8 @@ $autoload['libraries'] = array(
     'mongo',
     'session',
     'ui',
-    'user/ldap_user_plugin'
+    //----uncomment this line to auth from ldap
+    //'user/ldap_user_plugin'
     
 );
 ?>
