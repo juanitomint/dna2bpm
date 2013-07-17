b8ec91038e743f9ae24ce6ebdcfcee993b5498e5
astrid <astrid@lara>
2013-07-17 18:09:57 -0300

    cambios en los lenguajes y mensajes de la recuperacion de contraseña

--- application/modules/user/language/english/login_lang.php
+++ application/modules/user/language/english/login_lang.php
@@ -13,5 +13,22 @@ $lang["forgotPassword"] = "Forgot your username or password!";
 $lang["nolevel"] = "You don't have permission to access:";
 $lang["nousr"] = "You are not a registered user or you've misstyped either the username or password";
 $lang["hastolog"] = "You has to be logged to open:";
+
+$lang["mail"] = "Mail";
+$lang["PageDescriptionR"] = "Forgot Password";
+$lang["loginButtonR"] = "Send";
+$lang["loginMsgR"] = "Recover Password";
+$lang["nousrR"] = "You are not a registered user or you've misstyped your mail";
+$lang["mailsendpart1"]= "<h2>Dear user, </h2><p>We have received a request for password reset to its name.</p><p>Your username is:";
+$lang["mailsendpart2"]="<p>If you have been effected by simply click on the link at the bottom and You may choose your new password.</p>";
+$lang["mailsendpart3"]="I want to reset my password";
+$lang["mailsubject"]= "DNA2 password reset system";
+$lang["mailmsg1"]="Check your email. </br> Thanks.";
+$lang["mailback"]="BACK";
+$lang["mailform"]="New Password Form";
+$lang["mailpassno"]="Your password must be at least 5 characters.";
+$lang["mailmsg2"]="Your password has been changed successfully.</br>";
+$lang["mailalert"]="0, Error!";
+
 /* End of file user.php */
 /* Location: ./system/language/english/user.php */
--- application/modules/user/language/spanish/login_lang.php
+++ application/modules/user/language/spanish/login_lang.php
@@ -13,5 +13,27 @@ $lang['forgotPassword'] = 'Olvidó su usuario o su contraseña?';
 $lang['nolevel'] = 'Ud no posee los permisos necesarios para acceder a:';
 $lang['nousr'] = 'Ud no es un usuario registrado o confundió su nombre de usuario y contraseña';
 $lang['hastolog'] = 'Ud debe identificarse para acceder al recurso:';
+
+
+
+
+$lang["mail"] = "Mail";
+$lang["PageDescriptionR"] = "Olvido su contraseña";
+$lang["loginButtonR"] = "Enviar";
+$lang["loginMsgR"] = "Recuperar contraseña";
+$lang["nousrR"] = "Ud no es un usuario registrado o confundió su email";
+$lang["mailsendpart1"]= "<h2>Estimado usuario, </h2><p>Hemos recibido un pedido de reseteo de contraseña a su nombre.</p><p>Su nombre de usuario es:";
+$lang["mailsendpart2"]= "<p>Si ha sido efectuado por Ud. simplemente haga click en el link al pie y ud podr&aacute; elegir su nueva contraseña.</p>";
+$lang["mailsendpart3"]= "Quiero resetear mi clave";
+$lang["mailsubject"]= "Reseteo de contraseña sistema DNA2";
+$lang["mailmsg1"]="Revise su email.</br> Muchas gracias.";
+$lang["mailback"]="VOLVER";
+$lang["mailform"]="Nueva contraseña";
+$lang["mailpassno"]="Su contraseña debe tener al menos 5 car&aacute;cteres.";
+$lang["mailmsg2"]="Su contraseña ha sido cambiada exitosamente.</br>";
+$lang["mailalert"]="0, Ha habido alg&uacute;n error";
+
+
+
 /* End of file user.php */
 /* Location: ./system/language/spanish/user.php */
