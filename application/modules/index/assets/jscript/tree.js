fe20499be8764c92e178ba8602637bc20a2c0991
Juan Ignacio Borda <juanignacioborda@gmail.com>
2013-10-07 12:22:31 -0300

    restore save button

--- application/modules/index/assets/jscript/tree.js
+++ application/modules/index/assets/jscript/tree.js
@@ -2,9 +2,9 @@
 
 //---Refresh Tree
 var reloadTree = Ext.create('Ext.Action', {
-    text:'Reload',
-    iconCls:'x-tbar-loading',
-    handler:function(){
+    text: 'Reload',
+    iconCls: 'x-tbar-loading',
+    handler: function() {
         Ext.data.StoreManager.lookup('TreeStore').load();
         tree.uncheck_all()
     }
@@ -13,148 +13,142 @@ var reloadTree = Ext.create('Ext.Action', {
 var saveTree = Ext.create('Ext.Action', {
     iconCls: 'icon-save',
     text: 'Save',
-    
     handler: function(widget, event) {
         var records = tree.getView().getChecked(),
-        paths =new Array();
-                    
-        Ext.Array.each(records, function(rec){
+                paths = new Array();
+
+        Ext.Array.each(records, function(rec) {
             paths.push(rec.data.id);
         });
-                
+
 
         tree.setLoading('Saving');
         Ext.Ajax.request({
             // the url to the remote source
-            url: globals.module_url+'rbac_admin/repository/save',
+            url: globals.module_url + 'admin/save',
             method: 'POST',
             // define a handler for request success
-            params:{
+            params: {
                 //---get the active group
-                'idgroup':dataview.selModel.getLastSelected().data.idgroup,
-                'paths[]':paths
+                'paths[]': paths
             },
-            success: function(response, options){
+            success: function(response, options) {
                 tree.setLoading(false);
             },
             // NO errors ! ;)
-            failure: function(response,options){
-                alert('Error Loading:'+response.err);
+            failure: function(response, options) {
+                alert('Error Loading:' + response.err);
                 tree.setLoading(false);
-                
+
             }
         });
     }
 });
 
-    
+
 //---set all child checked
-function checkchange (node, checked) {
+function checkchange(node, checked) {
     node.eachChild(function(n) {
-        n.set('checked',checked);
-        if(n.childNodes.length){
-            checkchange(n,checked);
+        n.set('checked', checked);
+        if (n.childNodes.length) {
+            checkchange(n, checked);
         }
     });
 }
-function uncheck_all(){
-    root=tree.store.tree.getRootNode();
-    root.set('checked',false);
-    checkchange(root,false);
+function uncheck_all() {
+    root = tree.store.tree.getRootNode();
+    root.set('checked', false);
+    checkchange(root, false);
 }
 
 //---load checked nodes
-function load_checked(){
-    
+function load_checked() {
+
     tree.uncheck_all();
-    paths = [];                                  
+    paths = [];
     tree.setLoading('loading Checked');
     Ext.Ajax.request({
         // the url to the remote source
-        url: globals.module_url+'admin/getpaths',
+        url: globals.module_url + 'admin/getpaths',
         method: 'POST',
-        params:{
+        params: {
         },
         // define a handler for request success
-        success: function(response, options){
-            obj=Ext.decode(response.responseText);
-            obj.paths.forEach(function(path){
-                if(path.length){
-                    n=tree.store.tree.getNodeById(path);
-                    if(n)
-                        n.set('checked',true);
+        success: function(response, options) {
+            obj = Ext.decode(response.responseText);
+            obj.paths.forEach(function(path) {
+                if (path.length) {
+                    n = tree.store.tree.getNodeById(path);
+                    if (n)
+                        n.set('checked', true);
                 }
             });
             tree.setLoading(false);
         },
         // NO errors ! ;)
-        failure: function(response,options){
-            alert('Error Loading:'+response.err);
+        failure: function(response, options) {
+            alert('Error Loading:' + response.err);
             tree.setLoading(false);
-                
+
         }
     });
 }
 
 //---TREE
-var tree=Ext.create('Ext.tree.Panel', {
-    id:'PermTree',
-    title:'Permissions',
+var tree = Ext.create('Ext.tree.Panel', {
+    id: 'PermTree',
+    title: 'Permissions',
     store: Ext.data.StoreManager.lookup('TreeStore'),
     rootVisible: true,
     useArrows: true,
     frame: false,
-    animate:false,
-    stateful : true,
+    animate: false,
+    stateful: true,
     lines: true,
-    layout:'fit',
-    uncheck_all:uncheck_all,
-    load_checked:load_checked,
+    layout: 'fit',
+    uncheck_all: uncheck_all,
+    load_checked: load_checked,
     columns: [{
-        xtype: 'treecolumn', //this is so we know which column will show the tree
-        text: 'Folders',
-        flex: 2,
-        sortable: true,
-        dataIndex: 'text'
-    },
-    {
-        text: 'Path',
-        flex: 1,
-        dataIndex: 'id',
-        sortable: true
-    }
-    ],
-    listeners:{
-        checkchange:checkchange,
-            
-    },
-
-    dockedItems: [{
-        xtype: 'toolbar',
-        items: [
-            
-        
+            xtype: 'treecolumn', //this is so we know which column will show the tree
+            text: 'Folders',
+            flex: 2,
+            sortable: true,
+            dataIndex: 'text'
+        },
         {
-            xtype:'button',
-            enableToggle: true,
-            text:'[+]',
-            //iconCls:'x-tree-icon x-tree-icon-parent',
-            toggleHandler: function(button,state){
-                if(state){
-                    button.setText('[-]');
-                    //button.setIconCls('x-grid-tree-node-expanded  x-tree-icon-parent');
-                    tree.expandAll();
-                }else{
-                    button.setText('[+]');
-                    //button.setIconCls('x-tree-icon x-tree-icon-parent');
-                    tree.collapseAll();
-                }
-            }
-        //cls:'x-tree-icon x-tree-icon-parent'
-            
+            text: 'Path',
+            flex: 1,
+            dataIndex: 'id',
+            sortable: true
         }
-        ,
-        reloadTree
-        ]
-    }]
+    ],
+    listeners: {
+        checkchange: checkchange,
+    },
+    dockedItems: [{
+            xtype: 'toolbar', items: [
+                {
+                    xtype: 'button',
+                    enableToggle: true,
+                    text: '[+]',
+                    //iconCls:'x-tree-icon x-tree-icon-parent',
+                    toggleHandler: function(button, state) {
+                        if (state) {
+                            button.setText('[-]');
+                            //button.setIconCls('x-grid-tree-node-expanded  x-tree-icon-parent');
+                            tree.expandAll();
+                        } else {
+                            button.setText('[+]');
+                            //button.setIconCls('x-tree-icon x-tree-icon-parent');
+                            tree.collapseAll();
+                        }
+                    }
+                    //cls:'x-tree-icon x-tree-icon-parent'
+
+                }
+                ,
+                reloadTree,
+                saveTree
+            ]
+        }]
 });
