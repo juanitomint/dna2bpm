23b54b981fc474cd7f7ba739ba98d7e2d42882d5
Juan Ignacio Borda <juanignacioborda@gmail.com>
2014-03-26 16:06:52 -0300

    Change to get last token as state

--- application/modules/bpm/assets/types/state/kpi_controller.php
+++ application/modules/bpm/assets/types/state/kpi_controller.php
@@ -20,21 +20,31 @@ function state($kpi, $CI, $list = null) {
             $filter['status'] = array('$in' => (array) $filter['status']);
         }
     }
-    $tokens = $CI->bpm->get_tokens_byResourceId($kpi['resourceId'], $filter);
+    $filter['idwf'] = $kpi['idwf'];
+    $allcases = $CI->bpm->get_cases_byFilter($filter, array('id'));
+    $cases = array();
+    foreach ($allcases as $case) {
+        $token = $CI->bpm->get_last_token($kpi['idwf'], $case['id']);
+        if ($token) {
+            if ($token['resourceId'] == $kpi['resourceId']) { //---the case mathed
+                $cases[] = $case;
+            }
+        }
+    }
     $cpData = $kpi;
     $cpData['base_url'] = $CI->base_url;
     $cpData['module_url'] = $CI->module_url;
     //var_dump($tokens);
     //$cpData['tokens']=$tokens;
-    $cpData['count'] = count($tokens);
+    $cpData['count'] = count($cases);
     if (!$list) {
         $rtn = $CI->parser->parse('bpm/kpi_count', $cpData, true);
         return $rtn;
     } else { //----return cases matched
         //---map tokens to get case
-        $cases = array_map(function ($token) {
-            return $token['case'];
-        }, $tokens);
+        $cases = array_map(function ($case) {
+            return $case['id'];
+        }, $cases);
         return $cases;
     }
 }
