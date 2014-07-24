7cf113e5b662652ed692e9b32e481c8cae1bca98
Gabriel Fojo <gabriel@trialvd.com.ar>
2014-07-24 14:32:39 -0300

    logout link

--- application/modules/dashboard/views/layout.php
+++ application/modules/dashboard/views/layout.php
@@ -81,7 +81,7 @@
                                         <a href="{module_url}profile" class="btn btn-default btn-flat">Profile</a>
                                     </div>
                                     <div class="pull-right">
-                                        <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                        <a href="{base_url}user/logout" class="btn btn-default btn-flat">Sign out</a>
                                     </div>
                                 </li>
                             </ul>
