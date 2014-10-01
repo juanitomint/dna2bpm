f83f2c7cf893fed4744b5712ce83d1a4aea9907b
Gabriel Fojo <gabriel@trialvd.com.ar>
2014-10-01 15:07:10 -0300

    my bad..

---
+++ application/modules/inbox/views/msgs.php
@@ -0,0 +1,21 @@
+
+<!-- THE MESSAGES -->
+<input type="hidden" id="whereiam" value="{folder}" />
+<table class="table table-mailbox">
+	{if {folder}=='outbox'} {mymsgs}
+	<tr class="{read} msg tag_{tag}" data-msgid="{msgid}">
+		<td class="">{from_name}</td>
+		<td class="subject ">{subject}</td>
+		<td class="time">{msg_time}</td>
+	</tr>
+	{/mymsgs} {else} {mymsgs}
+	<tr class="{read} msg tag_{tag}" data-msgid="{msgid}">
+		<td class="small-col"><input type="checkbox" /></td>
+		<td class="small-col"><a class="{icon_star}" href="#"></a></td>
+		<td class="">{from_name}</td>
+		<td class="subject ">{subject}</td>
+		<td class="time">{msg_time}</td>
+	</tr>
+	{/mymsgs} {/if}
+</table>
+<div class="pull-right">{pagination}</div>
