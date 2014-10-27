bdb409a57c23ff1c3f6da8b7413260fcf4d60209
Juan Ignacio Borda <juanignacioborda@gmail.com>
2014-10-27 17:09:02 -0300

    Remove load user->model

--- application/modules/dashboard/controllers/dashboard.php
+++ application/modules/dashboard/controllers/dashboard.php
@@ -15,9 +15,7 @@ class Dashboard extends MX_Controller {
 
     function __construct() {
         parent::__construct();
-        $this->load->model('user/user');
         $this->load->config('dashboard/config');
-        $this->load->library('parser');
         $this->load->library('ui');
         $this->load->model('app');
         $this->load->model('bpm/bpm');
