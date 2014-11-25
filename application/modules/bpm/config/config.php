3ab8a53b5415e8e917476ed6cd66e9a8bc0db53d
Juan Ignacio Borda <juanignacioborda@gmail.com>
2014-11-25 17:21:12 +0000

    don't auto add admin

--- application/modules/bpm/config/config.php
+++ application/modules/bpm/config/config.php
@@ -14,8 +14,8 @@ $config['auto_create_groups']=true;
 options for admins:
  */
 // add me to the array of assigned  
-$config['auto_add_admin']=true;
+$config['auto_add_admin']=false;
 // replace the assignement with my id
-$config['auto_assign_admin']=true;
+$config['auto_assign_admin']=false;
 // Choose whether to make thumbnails or not
 $config['make_thumbnails']=true;
