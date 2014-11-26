56c1cae1fdb100ac760c72c8af17b768081d5cbe
Juan Ignacio Borda <juanignacioborda@gmail.com>
2014-11-26 11:33:00 +0000

    remove extra space

--- application/modules/gitmod/views/status.php
+++ application/modules/gitmod/views/status.php
@@ -1,5 +1,5 @@
 <div class="box {update_class}">
-<span class="hidden widget_url">{widget_url}</span>
+    <span class="hidden widget_url">{widget_url}</span>
     <div class="box-header">
         <h3 class="box-title">{title}</h3>
         <div class="box-tools pull-right">
