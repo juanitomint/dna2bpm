3253dea79b84418ba70ec24a37aad979168458ac
gabriel <gabriel@trialvd.com.ar>
2014-12-02 12:27:12 -0300

    ticket 17675

--- application/modules/inbox/config/config.php
+++ application/modules/inbox/config/config.php
@@ -1,5 +1,5 @@
 <?php
-$config['pagination_items_x_page']	= 20;
+$config['pagination_items_x_page']	= 2;
 $config['pagination_width']	= 5; 
 $config['pagination_always_visible']=false;
 $config['date_format_within24hs']='H:i';
