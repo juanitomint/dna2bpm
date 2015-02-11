7de357025786c71e7f9310f5274b0a67dd0d765f
gabriel <gabriel@trialvd.com.ar>
2015-02-11 17:38:29 -0300

    fix {

--- application/modules/inbox/views/inbox.php
+++ application/modules/inbox/views/inbox.php
@@ -16,7 +16,7 @@
                                                 <ul class="nav nav-pills nav-stacked">
                                                     <li class="header">{lang folders}</li>
                                                     <li id="bt_inbox" class="{if {folder}=='inbox'}active{/if}"><a href="{base_url}inbox/print_folder/inbox" class="ajax" data-target="inbox-list"><i class="fa fa-inbox"></i> {lang inbox} (<span class='inbox_count'>{inbox_count}</span>)</a></li>
-                                                    <li id="bt_outbox" class="{if {folder}=='outbox'}active{/if}"><a href="{base_url}inbox/print_folder/outbox" class="ajax" data-target="inbox-list"><i class="fa fa-mail-forward"></i> {lang sent} ({<span class='sent_count'>{inbox_count}</span>)</a></li>
+                                                    <li id="bt_outbox" class="{if {folder}=='outbox'}active{/if}"><a href="{base_url}inbox/print_folder/outbox" class="ajax" data-target="inbox-list"><i class="fa fa-mail-forward"></i> {lang sent} (<span class='sent_count'>{sent_count}</span>)</a></li>
                                                     <li id="bt_star" class="{if {folder}=='star'}active{/if}" ><a  href="{base_url}inbox/print_folder/star" class="ajax" data-target="inbox-list"><i class="fa fa-star"></i> {lang starred} (<span class='inbox_count'>{star_count}</span>)</a></li>
                                                     <li id="bt_trash" class="{if {folder}=='trash'}active{/if}"><a href="{base_url}inbox/print_folder/trash" class="ajax" data-target="inbox-list"><i class="fa fa-folder"></i> {lang trash} (<span class='inbox_count'>{trash_count}</span>)</a></li>
                                                 </ul>
