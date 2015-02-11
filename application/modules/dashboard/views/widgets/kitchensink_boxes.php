e0c446e133472e7f1830c6c54984ba4033744188
gabriel <gabriel@trialvd.com.ar>
2015-02-11 17:15:29 -0300

    and then....

---
+++ application/modules/dashboard/views/widgets/kitchensink_boxes.php
@@ -0,0 +1,14 @@
+<strong>box-icon: </strong> &lt;fontawesome class&gt;<br>
+<strong>box-class: </strong> <strong style="color:#f60">[</strong>box [(btn-primary|btn-danger|btn-info|btn-warning|box-success)] [box-solid]<strong style="color:#f60">]</strong><br>
+<strong>box-buttons: </strong><strong style="color:#f60">[</strong>[remove] [collapse]<strong style="color:#f60">]</strong><br>
+<strong>box-collapsed: </strong><strong style="color:#f60">[</strong>[(0|1)]<strong style="color:#f60">]</strong><br>
+<p>i.e.:</p>
+<ul class="list-unstyled">
+    <li>"box_icon":"fa-pie-chart",</li>
+    <li>"box_class":["box"],</li>
+    <li>"box_buttons":["remove","collapse"],</li>
+    <li>"box_collapsed":"0"</li>
+</ul>
+
+
+
