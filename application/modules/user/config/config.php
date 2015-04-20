4f79caff34ba70a7181d7f16c66ff1ebb7990fb4
Juan Ignacio Borda <juanignacioborda@gmail.com>
2015-04-20 12:15:30 -0300

    configurable autodiscover policy

--- application/modules/user/config/config.php
+++ application/modules/user/config/config.php
@@ -4,6 +4,8 @@ $config['show_warn']	= true;
 $config['run_mode']	= 'development';
 $config['default_controller']	= 'dashboard';
 $config['groupAdmin']	= 1;
+//----default autodicover policy set to false for production
+ $config['autodiscover']	= true;
 
 //----set if yopu want to use a plugin
 $config['user_plugin']=array(
