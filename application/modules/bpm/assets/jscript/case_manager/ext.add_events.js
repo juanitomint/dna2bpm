6f5000bfb661847b3a243a992b5ea2d03f963319
Juan Borda <juanignacioborda@gmail.com>
2015-10-11 16:59:55 -0300

    fix wrong position in lanes after subproccess

--- application/modules/bpm/assets/jscript/case_manager/ext.add_events.js
+++ application/modules/bpm/assets/jscript/case_manager/ext.add_events.js
@@ -1,53 +1,51 @@
 /*
  *  Add events for overlays
  */
-var add_events=function(shapes){
-    for (j in shapes){
-        var shape=shapes[j];
-        xBound=0;
-        yBound=0;
+var add_events = function(shapes) {
+    for (j in shapes) {
+        var shape = shapes[j];
+        xBound = 0;
+        yBound = 0;
         //console.log(shape.stencil.id,exclude_shape.indexOf(shape.stencil.id)==-1);
-        xBound=shape.bounds.upperLeft.x+offset_x; //( -1 to fix nested objects
-        yBound=shape.bounds.upperLeft.y+offset_y;                        
-        width=shape.bounds.lowerRight.x-shape.bounds.upperLeft.x;
-        height=shape.bounds.lowerRight.y-shape.bounds.upperLeft.y
-        if(exclude_shape.indexOf(shape.stencil.id)==-1){//---if not excluded
-            switch(shape.stencil.id){
+        xBound = shape.bounds.upperLeft.x + offset_x; //( -1 to fix nested objects
+        yBound = shape.bounds.upperLeft.y + offset_y;
+        width = shape.bounds.lowerRight.x - shape.bounds.upperLeft.x;
+        height = shape.bounds.lowerRight.y - shape.bounds.upperLeft.y
+        if (exclude_shape.indexOf(shape.stencil.id) == -1) { //---if not excluded
+            switch (shape.stencil.id) {
                 case 'Lane':
-                    width=24;
+                    width = 24;
                     break;
-                 
+
             }
-            add=new Array();
-            add.push('<span class="resourceId">resourceId:<br>'+shape.resourceId+'</span>');
-            if(shape.properties.tasktype){
-                switch(shape.properties.tasktype){
+            add = new Array();
+            add.push('<span class="resourceId">resourceId:<br>' + shape.resourceId + '</span>');
+            if (shape.properties.tasktype) {
+                switch (shape.properties.tasktype) {
                     case 'Script':
-                        add.push((globals.idcase) ? '<a target="_blank" class="btn btn-small btn-info" href="'+globals.base_url+'bpm/test/test_task/'+globals.idwf+'/'+globals.idcase+'/'+shape.resourceId+'"><i class="fa fa-flask fa-white"></i> Test Script</button>':'');
+                        add.push((globals.idcase) ? '<a target="_blank" class="btn btn-small btn-info" href="' + globals.base_url + 'bpm/test/test_task/' + globals.idwf + '/' + globals.idcase + '/' + shape.resourceId + '"><i class="fa fa-flask fa-white"></i> Test Script</button>' : '');
                         break;
-                    
+
                     case 'Send':
-                        add.push((globals.idcase) ? '<a target="_blank" class="btn btn-small btn-info" href="'+globals.base_url+'bpm/test/send/'+globals.idwf+'/'+globals.idcase+'/'+shape.resourceId+'"><i class="fa fa-envelope fa-white"></i> Test Msg</button>':'');
+                        add.push((globals.idcase) ? '<a target="_blank" class="btn btn-small btn-info" href="' + globals.base_url + 'bpm/test/send/' + globals.idwf + '/' + globals.idcase + '/' + shape.resourceId + '"><i class="fa fa-envelope fa-white"></i> Test Msg</button>' : '');
                         break;
-                    
-                } 
+
+                }
             }
-             
-            Ext.core.DomHelper.append( 'svg-box',
-            {
-                tag:'div',
-                cls:'model_overlay',
-                id: 'overlay'+shape.resourceId,
-                style:' border-radius: 3px;\n\
+
+            Ext.core.DomHelper.append('svg-box', {
+                tag: 'div',
+                cls: 'model_overlay',
+                id: 'overlay' + shape.resourceId,
+                style: ' border-radius: 3px;\n\
                         position:absolute;\n\
-                        left:'+xBound+'px;\n\
-                        top:'+yBound+'px;\n\
-                        width:'+width+'px;\n\
-                        height:'+height+'px;\n\
+                        left:' + xBound + 'px;\n\
+                        top:' + yBound + 'px;\n\
+                        width:' + width + 'px;\n\
+                        height:' + height + 'px;\n\
 \n\
 '
-            }
-            );
+            });
             config = {
                 id: 'toolTip' + shape.resourceId,
                 target: 'overlay' + shape.resourceId,
@@ -58,10 +56,10 @@ var add_events=function(shapes){
                 html: add.join('<br/>')
             };
             tooltips.push(
-                    Ext.create('Ext.tip.ToolTip', config)
-                    );
-                    
-            div=Ext.get('overlay'+shape.resourceId);
+                Ext.create('Ext.tip.ToolTip', config)
+            );
+
+            div = Ext.get('overlay' + shape.resourceId);
             /*
             div.on('mouseover',function(event,target,options){
                 this.dom.style.setProperty('background-color','green');
@@ -72,30 +70,31 @@ var add_events=function(shapes){
                 this.dom.style.removeProperty('opacity');
             });
             */
-            div.on('click',function(event,target,options){
-                resourceId=target.id.replace('overlay','');
-                tip=Ext.getCmp('toolTip' + resourceId);
-                if(tip.isVisible()){
-                Ext.getCmp('toolTip' + resourceId).autoHide = true;    
-                } else {
-                Ext.getCmp('toolTip' + resourceId).show(); 
-                Ext.getCmp('toolTip' + resourceId).autoHide = false;
+            div.on('click', function(event, target, options) {
+                resourceId = target.id.replace('overlay', '');
+                tip = Ext.getCmp('toolTip' + resourceId);
+                if (tip.isVisible()) {
+                    Ext.getCmp('toolTip' + resourceId).autoHide = true;
+                }
+                else {
+                    Ext.getCmp('toolTip' + resourceId).show();
+                    Ext.getCmp('toolTip' + resourceId).autoHide = false;
                 }
                 //console.log(resourceId);
-                tokenGrid.selModel.select(tokenGrid.store.find('resourceId',resourceId));
+                tokenGrid.selModel.select(tokenGrid.store.find('resourceId', resourceId));
             });
         }
-        flat[shape.resourceId]=shape;
-        if(shape.childShapes.length){
-            offset_x_orig=offset_x;
-            offset_y_orig=offset_y;
-            offset_x=xBound;
-            offset_y=yBound;
-                
+        flat[shape.resourceId] = shape;
+        if (shape.childShapes.length) {
+            var offset_x_orig = offset_x;
+            var offset_y_orig = offset_y;
+            offset_x = xBound;
+            offset_y = yBound;
             add_events(shape.childShapes);
             //---restore original valoues of offset
-            offset_x=offset_x_orig;
-            offset_y=offset_y_orig;
+            offset_x = offset_x_orig;
+            offset_y = offset_y_orig;
+
         }
     }
 }
