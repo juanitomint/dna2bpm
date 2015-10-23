e6915b7f205e3f0882d359be5e241a342b027544
Juan Borda <juanignacioborda@gmail.com>
2015-10-23 10:50:38 -0300

    User Layer Refactor WIP1

--- application/modules/user/libraries/Cimongo_plugin.php
+++ application/modules/user/libraries/Cimongo_plugin.php
@@ -2,7 +2,7 @@
 
 /**
  * This libray load cimongo and set db as mongo and apply bindings for replaced functions
- * 
+ *
  * @author Juan Ignacio Borda <juanignacioborda@gmail.com>
  */
 class Cimongo_plugin {
@@ -13,11 +13,18 @@ class Cimongo_plugin {
         if ($ci) {
             $ci->load->library('cimongo/cimongo');
             $ci->db = $ci->cimongo;
+            //---Load custom user class
+            $ci->load->library('user/cimongo_user_plugin');
+            $ci->user = $ci->cimongo_user_plugin;
         }
     }
 
     function apply() {
         return true;
+    }
+
+    function where_id($_id) {
+     return   array('_id' => new MongoId($_id));
     }
 
 }
