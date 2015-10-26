8b3fb9cf1e1815f43d04385739d3ad21d882ad63
Juan Borda <juanignacioborda@gmail.com>
2015-10-25 22:53:23 -0300

    minor fixes

--- application/modules/dna2/views/render_vista.php
+++ application/modules/dna2/views/render_vista.php
@@ -2,8 +2,8 @@
 $this->load->helper('html');
 $this->load->helper('url');
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
     <head>
         <title>View Form DNA&sup2;</title>
