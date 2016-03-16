1ee29f70107bfc5160e01e97c2201929b432bfbe
gabriel <gabriel@trialvd.com.ar>
2016-03-16 11:08:31 -0300

    avances

--- application/modules/ssl/controllers/ssl.php
+++ application/modules/ssl/controllers/ssl.php
@@ -79,6 +79,7 @@ class Ssl extends MX_Controller {
 
 
 
+//======================= ENCRYPT
 
     //=== Encryption (Public Key)
      public function encrypt($data=null,$fingerprint=null){
@@ -105,70 +106,16 @@ class Ssl extends MX_Controller {
      }   
 
     
-    //== @debug -> Public & Private Keys generation
-     public function new_keys(){
-        $config = array(
-            "digest_alg" => "sha512",
-            "private_key_bits" => 4096,
-            "private_key_type" => OPENSSL_KEYTYPE_RSA,
-        );
-        
-        // Create the private and public key
-        $res = openssl_pkey_new($config);
-        
-        // Extract the private key from $res to $privKey
-        openssl_pkey_export($res, $privKey);
-    
-        // Extract the public key from $res to $pubKey
-        $pubKey = openssl_pkey_get_details($res);
-        $pubKey = $pubKey["key"];
-
-        echo $privKey;
-        echo "\n";
-        echo $pubKey;  
-        
-    }   
-   
-
-     
+//======================= DECRYPT     
 
      
 //== Decryption for @debug
-     public function decrypt($privKey=""){
+public function decrypt($privKey=""){
 
 $encrypted=$this->encrypt("Hey como va","dc6fcd226959ffc26571ebd6c5697f43"); 
 //$encrypted64=base64_encode($encrypted);
 
-$pk=<<<_EOF_
------BEGIN RSA PRIVATE KEY-----
-MIIEowIBAAKCAQEA1Rfq2Czb/pX+nqN4NVPetvDVhMXQnE3SH2hwPBHO5Gn9raMp
-Rx2drP1U7FLRacwjdOOCGkmeK1BEc4+HIfltnaLIsGuJ87ghihNDTK7It3qIpkkd
-+eg7MZujaVeu0iI7R1DJNXV4tch+XMzjl4vLlESVqDaE5QNaTY/DjVJH328njL2d
-V5Z+xGSyaVB6V4LNXoe6YPyB7b5zomnqmT0I3Dy7cvAtDNTqHenzLHYk8zf0ziTy
-pK62fAmrQtC0Az/l1kRy9TP0uTm3coz8SIQzmjniVyzy0XPaXG1fhtMgYWaH0s4m
-/2SWldt01qFtE1UZ6aTySxEIbRhyQT7aEe9uzwIDAQABAoIBACvKinx6W4tqD7VS
-KrXq0m4N+BMdA83bQD8sG0R89GOEVJmGWkk/ENQoC3e1XUu8o9y1lFsKnfKQwEBv
-Unns5FXsyglXUDZBtMLHSqFLmfv6tnJVvE9LJj7/mQlg95A/cKcrNu5Bgdj5pt4z
-TOIr3F5P6eFzssPNeJVP4gP80MBdm17p5o0qlpQRcK32MHsm5pYsBSESrSxM/vcc
-diFYWrLfvhuaw8J2SBzCaV5XPNvaUkRPXLOM5ar5EjnRwHpZyLrAz+J0wmsEmgY3
-Few6Q7S+Xapni2wFdf0Lr1K+w+HEDB7GtRKgWcoenU7qu/f5w1OkxsZJAV99AJjt
-u28QKAECgYEA7+/ZKFLrVd4rm3OtHIDddKwG506neKFVewV/LiDRrZdvJGhNTRDX
-E8JO9YJUAYfiFcC5dWspBOpaVme7BtESNLk+EB1P8R4glOxymxqvhbwt4xvkFmtK
-hVy5qDqf+YOStblofHKlAOHX+CwKYTwJTAYMISV0e3eSyRw1ssXMk88CgYEA41wD
-TFvbEJg2t+5I14pSSVGDRVO5NDMzwzyE59mxdQ7tDHKONNAVDeElVCy+tCAnwW0J
-imYfaEbROA+MF0JBGPaF/ms6hZ/7nVaP2Lbpvcbky3BodJ+f+tpqHCViPeTmVjB1
-Bw40NcefSZ6m9Uiz+0Di5vxoBNouc0b6UgMnNQECgYB+APaI/6rJKGi8NW525pHm
-QKVLefnnFsreVU+p+OBEip96fjACRdK9dLCkq/HT7/liNRjwOfuLsksIz2bfuJIY
-ECQwsEQYOxsfOmEhZU1CLUXn2/DXeTbkfIKff1Id5eP4/UqK+GYA5ZnWocI9uBql
-yxM5oQLgCDaU2PS2UrwR7wKBgDYJMF99lVskUhz07SsNHPGABgr8ExBs+uh0AcJ4
-4sxHd991eobizZ423IBdAhYdblVybMoP63cFHcSNLWZ5wK8GKGKHaalIlKyYXifL
-kVIha09OsATHy2X7cyytVeQP+w6RBb9fiNkfUKRWqKezV9NnYIY1hyNgF5oeTHAh
-f7gBAoGBANcsGOxzxOhPTNJ2BseDs+pZaTfkd/mzD+Fy+hOteaZPm2LwrWtMFXqj
-MowTlhZUQSPz8ze0GttiD1wP4bcb+dRfkfSAOhh0GDEydCUaFz+HazvbxmzKiV8E
-QEW2GATCSKO/2J+ApxHFNabtydjUQctjxZVlhGYEJzPCG2ZuArAE
------END RSA PRIVATE KEY-----
-_EOF_;
-$privKey = openssl_get_privatekey($pk);
+$privKey = openssl_pkey_get_private($this->get_priv_key());
 
 
 openssl_private_decrypt($encrypted, $decrypted, $privKey);
@@ -176,13 +123,135 @@ openssl_private_decrypt($encrypted, $decrypted, $privKey);
 var_dump($decrypted);
 }
     
-  
+//======================= SIGNATURES
 
+//===== Create signature for data
 
-function test(){
-$this->load->library('encrypt');
+// function sign($data="Hey",$privKey=''){
+// //$privKey=$this->get_priv_key();
+// $privKey = openssl_pkey_get_private($privKey);
+// openssl_sign($data, $signature, $privKey, OPENSSL_ALGO_SHA256);
+// return $signature;
+
+// }
+
+//===== Verify Signature
+
+function verify($data,$signature,$pubKey){
+$signature=$this->sign();    
+//$pubKey=$this->get_pub_key();
+$pubKey = openssl_pkey_get_public($pubKey);
+$res=openssl_verify($data, $signature, $pubKey, "sha256WithRSAEncryption");
+var_dump($res);
 }
+  
     
+//======================= MISC 
+
+
+//== Public & Private Keys generation
+ public function new_keys(){
+    $config = array(
+        "digest_alg" => "sha512",
+        "private_key_bits" => 4096,
+        "private_key_type" => OPENSSL_KEYTYPE_RSA,
+    );
     
+    // Create the private and public key
+    $res = openssl_pkey_new($config);
+    
+    // Extract the private key from $res to $privKey
+    openssl_pkey_export($res, $privKey);
+
+    // Extract the public key from $res to $pubKey
+    $pubKey = openssl_pkey_get_details($res);
+    $pubKey = $pubKey["key"];
+
+    echo $privKey;
+    echo "\n";
+    echo $pubKey;  
+    
+}   
+
+//======================= DEBUG 
+    
+private function get_priv_key(){
+$ret=<<<_EOF_
+-----BEGIN PRIVATE KEY-----
+MIIJQwIBADANBgkqhkiG9w0BAQEFAASCCS0wggkpAgEAAoICAQC862qY8Paukk6q
+aqeHptRONSpf/sVxvUg2vMLRkY1Tt2ztZO2YflkIFrvuo8CQ+UqtZYC/jv91V47n
+BDLy9d1G025DVz6ep8EeYnVHLH/OGPQF8AG217jPQLGOVHEpqXq0/yrK964rkUME
+rLzX9xWFiQPn91RDyiEJECjm78B3/nKV229fPVv8LWJgZKT0hlM5Ndlnlvy1Wfto
+MULkExZ7NxXhZyp770MEvgpIaCgCd/zDENzMdVygDl/nMpBhYjV9kFCjkmX782/Y
+FfXsCdgpCNzn68UBbbnjircEuHdFb5jmYV1X1vuM2BfDdv7h1qtJ+8g1Kys1pHlS
+Uq/KHyC7em15BWAcBnDRTKBndmZKexT7Gu2u6m/iMAtjqkrwgaI6q9UZQmqQl9wF
+K97SfGm/7HQYqJMIFNF2w1JGfghz5eaJBpyRi1b84A7TIWSPCyuHtvsQyzp/lFjV
+GYMByANDXtVW/SoFYzKTmBWTzFKHZ/PYR+n7X4n1tbZlM9tQnvuDlioYN5zevpqg
+/vFl6SHlLwCyNVUv+k4msPG0hS/ak23hZ1EqCbEgTBVf4XA3tgQkoMGwkoSgmrWc
+Ev+Jqg/jflqFFJ4RInSpz5Ytuyt97MkVHAj9+6XZ8PMSvj79CC5gcF7KcwqIkM/x
+zXis524IBLwA+qKlI5zg4V/nb9PF3QIDAQABAoICAQCgUifkIpk1DHQ1qs0BEVKa
+DAQp8ssb+sWNriKmNh75LHAEBeKbxp1HgAN8QPrqDKSTAdCAv5mKuwNKiQWzqXJe
+6I5qF1/MxA/e+S8Nd3X+MK7lWwgZYngzaXNEleRytqCreN+X3xivYFa/YNfhbo2V
+BPGLqRiZbq8aQdx0HoH3Urdq9Q1OhYPKPbl+k+pomJWHt2sHvdo3DGWNxkA9zPUH
+G3g2EJa9Fg6Jt7yCNeG+NpUJ9QVrfS1dptU8HmzoN6+tHwtPxK1HYw9HEVvhM3l9
+WUgeiLZmv/dAYjduihytkEbxOy87m/VdUQTZm+IxRTuXTVRZyoigvTM36XPE0yWy
+fVx5D1P3T4LPpVSz/Vy/7fJeFwL0bWBqBAvngemPW7IssaXdxllDy+uSkEdf47EZ
+LGi6FDqQkv/h7IJWPowESbCceCW2qY3DNv6nqyAjkvu8kqlPmHF3p/QZO+psfXni
+xQtqqs7N8ewPrkSz01+DqUNYu0XB/g0nBN0ww/hFc5qlGC50dWoCVRaq4Tl4S2Wb
+BbVheNd5C5XNbAqPffc41t7oy/eIRMF4lKbthKKMN3+k6ehEXOoNhjO2j/TRt6hV
+VhRjagJYbgAmWq8OhRq5fwgb+hIztCUwXAo+H8Yb4E06K/Gk8ByVBeLQIGOdqoeo
+1Oo9yNjynTrk/O7DqJxLAQKCAQEA70jks2CxGgF72ltb0NtREVQwQcMC1WbtRAZa
+MwiQZjkA/DQrwYFYiHIVuPMJWe2zBhUOUv31Ha7cUqycPStrgyyqLZi5mshJ4dhI
+g+Z/gAUEDncloalYItOth9+9yIn8BkdBqI8N+084PThDQd3FmLho1ukxlEIMQPAz
+OpHurvfzOogfCUNwrsDAGlhPQd3WwfvEcU/+KGpILshbkJpN+r0QzH4nEKb0RztP
+lbf7cIDdaU9KMtzET8LH9Akb2jozYGNDlYf8VmpDgC3XY8PNHIPbjWNGFWFOPmpq
+RJIEJXGYHJfrAxzP2vY6swf4jqOPszirNQpuZZhB06Ktf0yX/wKCAQEAyh3ZDZew
+33/7aUPAJehfSuJSBcrgl3Ft1MsYuJR8qINnxaM2pz/IPSbsOj113JaT+0dLv2SX
+eBcqSnpY8Sp/TPo12/Ns7sT+F4oQdkacH3r9jYh5cSXZQnqLNuMBh/zQCpH87KUE
+EeUd9W5kKutfFLk3ebNSwvZvHiUZ9ZAKiVktJCjWjHp2UIvP2A7OwCsiQjpdM/9e
+JE6w1plQFBMWZoA1c8oUqlbHKrWGOfhg+b+J5cjL+97A99442rZY/FhtQIkby80C
+5iVoixxwBn9Dsjb1yogMawORSfqLaXQi1eF8DzrdOD5qfCXpwh6FvHxQasKWDV7K
+FU1gDHhKCNUCIwKCAQEApQgRM+YsP1NmqGL6IEIi12DJJ5HoEma6nYAEFc6CSP5n
+v4n746nh9bk5YiW8/VkDb9510qd0ttQzAJIr78RSklXrySbcW/RngGw7Fz2SEilj
+ctaaDbVOJDb6KAwYSIdiWrIqhqajbgBlOVPkjzj1Xy9Qn2iV2Tr0WJVRv8OGawZ3
+qpbXUPxCa0RlOcZOY48s3v1VrxEMqbMjtaBaBpFl1tkvDNq09rcvIzG04f3SXPWD
+v24ALrQ0cQ5V+emOXCRn6sKLikYPs0n25CC5vQT+IfyPICSn4XcLD+E1CbXrRifi
+UVY1sB/e+5V3RqLouvfz1BfxfNOE8GzieaRpJLMe4QKCAQAY5OQ/EFfwr0rABGA+
+SwixqH6ByCMxg/8LHpjE40UXXFgDt76biveW0Jx37+n8aW+Am59wy2r8l53V3ovl
+6F4VlRvdI3ZfUlQZgh/U8Y15MyTXsd+DWC4SShWrhPpDTZgyNRj57Lk7mwS9ngMo
+ZiUn4Eg87SFccg3toJQ58qvZjupIcd87HjpEYXQQIILGmIl3rsicBvAJeMe/JtL+
+Jfu0VEZBJLq24ElXsVP+/+Cx4i2R59F4DE+oN+64wYzkR9/s+vY5e97HigP+XlpN
+8o7b/Hj5oRgmdiIHApz5OByySzuEhLOKoeVrtR+9kRTXylf9Tko9C1fIl+ckogw8
+WkARAoIBAEu7EDl18nJtnNLmQhJ+ODxGHMSalYeOO2eKuGNX3+R9JlP2kPPXC/Qz
+0lhEZIyG/aIWE0CjZJJpa2KKqsKWuoyi5PRW1xqHvSybwLPeb5S7b+UlOmHCsm9E
+a/9WmeJEyPFQjb0sj/sy4XA3g9QQIb7TiREQcXL+/wqbEXfpYWialzpZbyDNVWNZ
+p0bf6Cyc1hedLHFcRRfCYS3NkI3H7zhslR/Gy+Mq/Yh10NteWR8H8JIwDaIJyRWm
+H1GEZCleP+7BPAk6enHrf6D0T20WtSGWCxTlF0S88NSHh7Ue8qC4MJwPJt2V7jTz
+db0O1GiOXIc321MquOq8a7xFMal00uU=
+-----END PRIVATE KEY-----
+_EOF_;
+
+return $ret;
+}
+
+private function get_pub_key(){
+$ret=<<<_EOF_
+-----BEGIN PUBLIC KEY-----
+MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAvOtqmPD2rpJOqmqnh6bU
+TjUqX/7Fcb1INrzC0ZGNU7ds7WTtmH5ZCBa77qPAkPlKrWWAv47/dVeO5wQy8vXd
+RtNuQ1c+nqfBHmJ1Ryx/zhj0BfABtte4z0CxjlRxKal6tP8qyveuK5FDBKy81/cV
+hYkD5/dUQ8ohCRAo5u/Ad/5yldtvXz1b/C1iYGSk9IZTOTXZZ5b8tVn7aDFC5BMW
+ezcV4Wcqe+9DBL4KSGgoAnf8wxDczHVcoA5f5zKQYWI1fZBQo5Jl+/Nv2BX17AnY
+KQjc5+vFAW2544q3BLh3RW+Y5mFdV9b7jNgXw3b+4darSfvINSsrNaR5UlKvyh8g
+u3pteQVgHAZw0UygZ3ZmSnsU+xrtrupv4jALY6pK8IGiOqvVGUJqkJfcBSve0nxp
+v+x0GKiTCBTRdsNSRn4Ic+XmiQackYtW/OAO0yFkjwsrh7b7EMs6f5RY1RmDAcgD
+Q17VVv0qBWMyk5gVk8xSh2fz2Efp+1+J9bW2ZTPbUJ77g5YqGDec3r6aoP7xZekh
+5S8AsjVVL/pOJrDxtIUv2pNt4WdRKgmxIEwVX+FwN7YEJKDBsJKEoJq1nBL/iaoP
+435ahRSeESJ0qc+WLbsrfezJFRwI/ful2fDzEr4+/QguYHBeynMKiJDP8c14rOdu
+CAS8APqipSOc4OFf52/Txd0CAwEAAQ==
+-----END PUBLIC KEY-----
+_EOF_;
+return $ret;
+}
     
 }//class
