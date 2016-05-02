900470a66733f00e9e161f27b1c06d5497e9992c
Juan Ignacio Borda <juanignacioborda@gmail.com>
2016-05-02 12:30:47 -0300

    removed undefined $last_update

--- application/modules/bpm/controllers/Repository.php
+++ application/modules/bpm/controllers/Repository.php
@@ -193,7 +193,7 @@ class Repository extends MX_Controller {
         );
         $data = ($mywf['data']) ? $mywf['data'] : $template;
         if (!$debug){
-            $this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s', $last_update).' GMT');
+            // $this->output->set_header('Last-Modified: '.gmdate('D, d M Y H:i:s', $last_update).' GMT');
             $this->output->set_header("Cache-Control: no-store, no-cache, must-revalidate");
             $this->output->set_header("Cache-Control: post-check=0, pre-check=0");
             $this->output->set_header("Pragma: no-cache");
