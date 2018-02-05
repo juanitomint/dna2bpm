07ce6caee3a8eec3ab4a71c72c3a0dd3927b0230
Juan Borda <juanignacioborda@gmail.com>
2018-02-04 22:23:32 -0300

    bpm requieres user to be logged in

---
+++ application/libraries/Assets_restricted.php
@@ -0,0 +1,74 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+/*
+ * ASSETS Controller
+ * This file allows you to  access assets from within your modules directory
+ * 
+ * @author Borda Juan Ignacio
+ * 
+ * @version 	1.0 (2018-02-04)
+ * 
+ */
+
+class Assets extends MX_Controller {
+
+    function __construct() {
+        parent::__construct();
+        if(!$this->user->isloggedin())
+            show_error('Access restricted');
+        
+    }
+    
+    function index(){
+        //$this->user->authorize();
+         
+         if(count($this->uri->segments)==2){
+             show_error("Serving assets for: ".APPPATH. 'modules/' . implode('/', $this->uri->segments));
+             exit;
+         }
+        //---get working directory and map it to your module
+        $file = APPPATH. 'modules/' . implode('/', $this->uri->segments);
+        //----get path parts form extension
+        $path_parts = pathinfo( $file);
+        //---set the type for the headers
+        $file_type=  strtolower($path_parts['extension']);
+        
+        if (is_file($file)) {
+            //----write propper headers
+            switch ($file_type) {
+                case 'css':
+                    header('Content-type: text/css');
+                    break;
+
+                case 'js':
+                    header('Content-type: text/javascript');
+                    break;
+                
+                case 'json':
+                    header('Content-type: application/json;charset=UTF-8');
+                    break;
+                
+                case 'xml':
+                   header('Content-type: text/xml');
+                    break;
+                
+                case 'pdf':
+                  header('Content-type: application/pdf');
+                    break;
+                
+                case 'jpg' || 'jpeg' || 'png' || 'gif':
+                    header('Content-type: image/'.$file_type);
+                    break;
+            }
+ 
+            readfile($file);
+        } else {
+            show_error("Asset not found: $file");
+        }
+        exit;
+    }
+
+}
--- application/modules/bpm/controllers/Assets.php
+++ application/modules/bpm/controllers/Assets.php
@@ -2,6 +2,6 @@
 
 if (!defined('BASEPATH'))
     exit('No direct script access allowed');
-
+$auth=false;
 // include the global Assets class
-include(APPPATH.'libraries/Assets.php');
+include(APPPATH.'libraries/Assets_restricted.php');
