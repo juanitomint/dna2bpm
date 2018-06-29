5ff650ed01b336f15f9b52183429008d5049e102
Juan Borda <juanignacioborda@gmail.com>
2018-06-29 12:35:12 -0300

    added more params to js globals

--- application/modules/formio/controllers/Formio.php
+++ application/modules/formio/controllers/Formio.php
@@ -62,7 +62,6 @@ class Formio extends MX_Controller {
             $this->load->model('bpm/bpm');
             ///---get resrourceId
             $token=$this->bpm->get_token_byid($token_id);
-            
             $case=$this->bpm->get_case($idcase,$idwf);
             //--- Check if key exists
             if (isset($case['data']['external']['formio'][$token['resourceId']])) {
@@ -72,8 +71,9 @@ class Formio extends MX_Controller {
         $customData['global_js']['idwf']=  $idwf;
         $customData['global_js']['idcase']= $idcase;
         $customData['global_js']['token_id']= $token_id;
+        $customData['global_js']['title']= $token['title'];
         $customData['global_js']['action']="http://localhost:3001/form/$form_id/submission/?live=1";
-        // $customData['global_js']['action_post']=$this->base_url."bpm/engine/post/$token_id/formio/";
+        $customData['global_js']['action_post']=$this->base_url."bpm/engine/post/$token_id/formio/";
             //  var_dump($customData);exit;
 	    //----pasar params al dashboard usar la url para el render 
 		Modules::run('dashboard/dashboard', 'formio/json/formio_poc.json',null,$customData);
